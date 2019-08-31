eed016c7f02ceca67664836ec98fe748b888330e
Anton Briganti <anton.briganti@gmail.com>
2019-09-01 02:05:10 +1000

    added tally of cards drawn, started tracking sangan+4

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from pprint import pprint
 deck = []
 with open("source.txt") as file:
    for line in file:
@@ -6,17 +7,23 @@ with open("source.txt") as file:
             deck.append(line.strip("\n"))
 
 flag = True   
-target = ["Exodia the Forbidden One", "Left Arm of Forbidden One", "Left Leg of the Forbidden One", "Right Arm of the Forbidden One", "Right Leg of the Forbidden One"]
-
+target = set(["Exodia the Forbidden One", "Left Arm of Forbidden One", "Left Leg of the Forbidden One", "Right Arm of the Forbidden One", "Right Leg of the Forbidden One"])
+sangan = target.union(set(["Sangan"]))
+tally = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, "sangan + 4": 0, 5: 0}
 count = 0
+
 while flag:
     shuffle(deck)
-    hand = deck[:5]
-    # change to sets, use to call out when something's close/got one piece
-    if target == sorted(hand):
-        print(count)
+    hand = set(deck[:5])
+    tally[len(target.intersection(hand))] += 1
+    if len(sangan.intersection(hand)) == 5:
+        tally["sangan + 4"] += 1
+    if len(target.intersection(hand)) == 5:
         break
     count+=1
+
+print("total hands drawn:", count)
+pprint(tally, width=1)
 
 
            
