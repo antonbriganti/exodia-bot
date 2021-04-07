72f8abb10661ac3fe7f9cb0ca985cccd622216b2
Anton Briganti <anton.briganti@gmail.com>
2021-04-08 00:17:47 +1000

    small refactor to make things neater

--- script.py
+++ script.py
@@ -1,33 +1,45 @@
 from random import shuffle
 from pprint import pprint
-deck = []
-with open("source.txt") as file:
-   for line in file:
-       if (line.strip("\n") != ""):
-            deck.append(line.strip("\n"))
- 
+
+def create_deck_from_file(file_location):
+    deck = []
+
+    with open(file_location) as file:
+        for line in file:
+            if (line.strip("\n") != ""):
+                    deck.append(line.strip("\n"))
+    
+    return deck
+
+def print_results(tally, total_hands_drawn):
+    print()
+    print("total hands drawn :", total_hands_drawn)
+    print("it would take", total_hands_drawn/2, "hours to draw get this hand")
+    print("that's", total_hands_drawn/2/24/7/52, "years")
+    pprint(tally, width=1)
+
+deck = create_deck_from_file("source.txt")
 target = set(["Exodia the Forbidden One", "Left Arm of Forbidden One", "Left Leg of the Forbidden One", "Right Arm of the Forbidden One", "Right Leg of the Forbidden One"])
-tally = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, "sangan + 4": 0, 5: 0}
-count = 0
+exodia_hands_draw_tally = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, "sangan + 4": 0, 5: 0}
+total_hands_drawn = 0
+
+print("-- starting exodia bot --")
 
 while True:
+    total_hands_drawn+=1
+    
     shuffle(deck)
     hand = set(deck[:5])
 
-    count+=1
+    amount_of_exodia_cards_drawn = len(target.intersection(hand))
 
-    tally[len(target.intersection(hand))] += 1
-    if len(target.intersection(hand)) == 4 and 'Sangan' in hand:
-        tally["sangan + 4"] += 1
-        print("drew sangan+4 on hand", count, "using hand", hand)
-    if len(target.intersection(hand)) == 5:
-        break
+    exodia_hands_draw_tally[amount_of_exodia_cards_drawn] += 1
     
+    if amount_of_exodia_cards_drawn == 4 and 'Sangan' in hand:
+        exodia_hands_draw_tally["sangan + 4"] += 1
+        print("drew sangan+4 on hand", total_hands_drawn, "using hand", hand)
+    
+    if amount_of_exodia_cards_drawn == 5:
+        break
 
-print("total hands drawn :", count)
-print("it would take", count/2, "hours to draw get this hand")
-print("that's", count/2/24/7/52, "years")
-pprint(tally, width=1)
-
-
-           
+print_results(exodia_hands_draw_tally, total_hands_drawn)
